ab7da309e9aead936f11a4544c8853866112628a
Duc Truong <ductr@icloud.com>
2020-04-11 23:27:21 -0700

    Add files via upload

---
+++ GCExchange/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GCExchange
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Create an instance of class User == object 
+			var User1 = new User();
+			User1.Name = "Jason Bateman";
+			User1.EmailAddress = "jason@gmail.com";
+			User1.BankAccountNo = 123456;
+			User1.BankRoutingNo = 111110;
+			User1.Address = "123 King Street";
+			User1.Deposit(100);
+
+			var User2 = new User();
+			User1.Name = "Helen Pierce";
+			User1.EmailAddress = "helen@msn.com";
+			User1.BankAccountNo = 2141452;
+			User1.BankRoutingNo = 3365525;
+			User1.Address = "456 Downing Avenue";
+		}
+	}
+}
---
+++ GCExchange/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCExchange
+{
+	class User
+	{
+		#region Properties
+
+		public string Name { get; set; }
+		public string EmailAddress { get; set; }
+		public int BankAccountNo { get; set; }
+		public int BankRoutingNo { get; set; }
+		public string Address { get; set; }
+		public decimal Balance { get; set; }
+
+		#endregion
+
+		#region Methods
+
+		public void Deposit(decimal amount)
+		{
+			Balance += amount;
+		}
+
+		public decimal Withdraw(decimal amount)
+		{
+			Balance -= amount;
+			return Balance;
+		
+		}
+
+		
+		#endregion
+
+	}
+
+}
