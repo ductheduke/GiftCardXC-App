35d744302b7bc6f5842b4c0131b8f205bb24bf2f
Duc Truong <ductr@icloud.com>
2020-04-15 23:24:52 -0700

    Create GC.cs

---
+++ GCExchange/GC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCExchange
+{
+	static class GC
+	{
+		/// <summary>
+		/// Create a user account in the Gift Card Xchange App
+		/// </summary>
+		/// <param name="userName">Name of the user</param>
+		/// <param name="emailAddress">Email address of the user</param>
+		/// <param name="userType">Type of user</param>
+		/// <returns></returns>
+		public static User Register(string userName,
+									string emailAddress,
+									TypeOfUser userType)
+		{
+			var user = new User
+			{
+				UserName = userName,
+				EmailAddress = emailAddress,
+				UserType = userType
+			};
+			return user;
+		}
+	}
+}
