bcd7253e05293eb5a7dabb4412cd5106969ceaf7
Duc Truong <ductr@icloud.com>
2020-04-15 23:25:00 -0700

    Update Program.cs

--- GCExchange/Program.cs
+++ GCExchange/Program.cs
@@ -7,20 +7,18 @@ namespace GCExchange
 		static void Main(string[] args)
 		{
 			//Create an instance of class User == object 
-			var User1 = new User();
-			User1.Name = "Jason Bateman";
-			User1.EmailAddress = "jason@gmail.com";
-			User1.BankAccountNo = 123456;
-			User1.BankRoutingNo = 111110;
-			User1.Address = "123 King Street";
-			User1.Deposit(100);
+			var FirstUser = GC.Register("Jason Bateman", "jason@gmail.com", TypeOfUser.Buyer);
+			FirstUser.BankAccountNo = 123456;
+			FirstUser.BankRoutingNo = 111110;
+			FirstUser.Address = "123 King Street";
+			FirstUser.Deposit(100);
+			Console.WriteLine($"User ID: {FirstUser.UserID}, User Name: {FirstUser.UserName}, User Type: {FirstUser.UserType}, Email: {FirstUser.EmailAddress}");
 
-			var User2 = new User();
-			User1.Name = "Helen Pierce";
-			User1.EmailAddress = "helen@msn.com";
-			User1.BankAccountNo = 2141452;
-			User1.BankRoutingNo = 3365525;
-			User1.Address = "456 Downing Avenue";
+			var SecondUser = GC.Register("Helen Pierce", "helen@msn.com", TypeOfUser.Seller);
+			SecondUser.BankAccountNo = 2141452;
+			SecondUser.BankRoutingNo = 3365525;
+			SecondUser.Address = "456 Downing Avenue";
+			Console.WriteLine($"User ID: {SecondUser.UserID}, User Name: {SecondUser.UserName}, User Type: {SecondUser.UserType}, Email: {SecondUser.EmailAddress}");
 		}
 	}
 }
