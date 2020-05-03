3e759d99a8951956165900b56e4a808252082ef7
Duc Truong <ductr@icloud.com>
2020-05-02 23:07:57 -0700

    Create Transaction.cs

---
+++ GCExchange/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCExchange
+{
+	enum TypeOfTransaction
+	{
+		Credit,
+		Debit
+	}
+
+	class Transaction
+	{
+		public int TransactionID { get; set; }
+		public DateTime TransactionDate { get; set; }
+		public decimal Amount { get; set; }
+		public TypeOfTransaction TransactionType { get; set; }
+		public int UserID { get; set; }
+	}
+}
