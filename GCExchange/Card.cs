dd57652bb893483abd90d164a4885ac7ef231c3a
Duc Truong <ductr@icloud.com>
2020-05-02 23:08:07 -0700

    Create Card.cs

---
+++ GCExchange/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCExchange
+{
+	class Card
+	{
+		private static int lastCardID = 0;
+
+		#region Properties
+		public int CardID { get; private set; }
+		public string VendorName { get; set; }
+		public decimal CardAmount { get; set; }
+		public string EmailAddress { get; set; }
+		public DateTime ListingDate { get; private set; }
+		#endregion
+
+		#region Constructor
+		public Card()
+		{
+			CardID = lastCardID++;
+			ListingDate = DateTime.Now;
+		}
+		#endregion
+	}
+}
